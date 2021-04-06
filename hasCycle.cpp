714ecda0fd398a81292725d94d29a2530e0f7762
peng <2460819991@qq.com>
2021-04-06 19:18:58 +0800

    Leetcode -- 141. 环形链表

---
+++ hasCycle.cpp
@@ -0,0 +1,30 @@
+// ÌâÄ¿Á´½Ó£ºhttps://leetcode-cn.com/problems/linked-list-cycle/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (head == nullptr)    return false;
+        
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while (fast && fast->next)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (fast == slow)
+            {
+                return true;
+                break;
+            }
+        }
+        return false;
+    }
+};

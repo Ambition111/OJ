3c3ccb59b5c7d3fff8a5b9b68c38806d768e9569
peng <2460819991@qq.com>
2021-06-03 21:25:19 +0800

    牛客题霸 -- 字符串变形

---
+++ trans.cpp
@@ -0,0 +1,36 @@
+// ÌâÄ¿Á´½Ó£ºhttps://www.nowcoder.com/practice/c3120c1c1bc44ad986259c0cf0f0b80e
+
+class Solution {
+public:
+    string trans(string s, int n) {
+        string str;
+        if (s.empty())    return str;
+        
+        int j = n - 1;
+        int i = j;
+        while (i >= 0)
+        {
+            while (s[i] != ' ' && i >= 0)
+            {
+                if (islower(s[i]))
+                {
+                    s[i] -= 32;
+                    --i;
+                }
+                else if (isupper(s[i]))
+                {
+                    s[i] += 32;
+                    --i;
+                }
+            }
+            int tmp = i + 1;
+            while (tmp <= j)
+            {
+                str += s[tmp++];
+            }
+            str += s[i--];
+            j = i;
+        }
+        return str;
+    }
+};
